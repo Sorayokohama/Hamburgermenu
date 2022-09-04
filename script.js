d40c6620bf71c3b4b10b890b33c255b95ec7480e
Sorayokohama <87938508+Sorayokohama@users.noreply.github.com>
2022-09-04 21:45:06 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.').onclick = function (e) {
+  var menu = document.querySelector('.menu');
+  var  = document.querySelector('.');
+
+  menu.classList.toggle('is-active');
+  .classList.toggle('is-active');
+
+  e.preventDefault();
+}
