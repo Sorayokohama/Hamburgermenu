b7d63870a9b83b8875f527dac820b5a5449a3b15
Sorayokohama <87938508+Sorayokohama@users.noreply.github.com>
2022-09-05 17:43:00 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.').onclick = function (e) {
-  var menu = document.querySelector('.menu');
-  var  = document.querySelector('.');
-
-  menu.classList.toggle('is-active');
-  .classList.toggle('is-active');
-
-  e.preventDefault();
-}
